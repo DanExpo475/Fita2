ec16b178eac1089a918940cdc3b8eb9134761ba3
DanExpo475 <148785018+DanExpo475@users.noreply.github.com>
2023-11-24 12:42:58 +0100

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('DB_HOST', 'localhost');  
-define('DB_USER', 'usuario_db'); 
-define('DB_PASSWORD', 'contraseña_db'); 
-define('DB_NAME', 'nombre_db'); 
-define('DB_CHARSET', 'utf8mb4'); 
-$debug_mode = true; 
-?>
