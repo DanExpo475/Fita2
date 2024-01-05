9e6864dd53670b682407f93478f319dc607d89e4
DanExpo475 <148785018+DanExpo475@users.noreply.github.com>
2024-01-05 13:11:52 +0100

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('DB_HOST', 'mysql-carlaaramirez.alwaysdata.net');  
-define('DB_USER', '333404'); 
-define('DB_PASSWORD', 'Danielcarla12345*'); 
-define('DB_NAME', 'carlaaramirez_todoapp'); 
-define('DB_CHARSET', 'utf8mb4'); 
-$debug_mode = true; 
-?>
